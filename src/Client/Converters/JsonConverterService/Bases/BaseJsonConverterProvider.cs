bfe586ac19c9ec734d8d30f13ebee62fcdf7e368
ZLoo <Kozlov.s.v.1992@gmail.com>
2024-07-28 12:06:16 +0300

    File organization

--- src/Client/Converters/JsonConverterService/Bases/BaseJsonConverterProvider.cs
+++ src/Client/Converters/JsonConverterService/Bases/BaseJsonConverterProvider.cs
@@ -1,11 +1,11 @@
 using System.Text.Encodings.Web;
 using System.Text.Json;
 
-namespace JsonConverterService
+namespace JsonConverterService.Bases
 {
     public abstract class BaseJsonConverterProvider
     {
-        protected virtual JsonSerializerOptions Options { get; set; } = new JsonSerializerOptions()
+        protected virtual JsonSerializerOptions Options { get; set; } = new JsonSerializerOptions
         {
             Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
             WriteIndented = true
--- src/Client/Converters/JsonConverterService/JsonConverterProviderAsync.cs
+++ src/Client/Converters/JsonConverterService/JsonConverterProviderAsync.cs
@@ -3,6 +3,8 @@ using System.IO;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using JsonConverterService.Bases;
+using JsonConverterService.Interfaces;
 
 namespace JsonConverterService
 {
