53937b33a407fc119536265b105fa7382e9af010
ZLoo <Kozlov.s.v.1992@gmail.com>
2024-07-30 22:29:03 +0300

    draft

--- src/Tests/Servers/MiniGrpcServer/Program.cs
+++ src/Tests/Servers/MiniGrpcServer/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Core;
+using GrpcModels.Protos;
+using MiniGrpcServer.GrpcServices;
+
 Console.WriteLine("Hello, World!");
+
+var port = 9007;
+
+var server = new Server
+{
+    Services = { TestGrpcService.BindService(new TestService()) },
+    Ports = { new ServerPort("localhost", port, ServerCredentials.Insecure) }
+};
+server.Start();
+
+Console.WriteLine("gRPC server listening on port " + port);
+Console.ReadKey();
+
+server.ShutdownAsync().Wait();
